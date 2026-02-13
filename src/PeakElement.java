791776cc80ff42e4ea58ccffa6e761b9e27b4929
SREEVINAYAKG <srivinayakg2005@gmail.com>
2026-02-13 21:48:04 +0530

    Submitted peakelement

--- src/PeakElement.java
+++ src/PeakElement.java
@@ -1,7 +1,28 @@
+import java.util.*;
 public class PeakElement {
-    
-        // TODO: Read n
-        // TODO: Read the array elements
-        // TODO: Find the first peak element and print its index
-    
+
+        public static int findPeak(int[] arr,int n){
+                int ans = -1;
+                if(arr[0]>arr[1]) return 0;
+                for(int i=1;i<n-1;i++){
+                        if(arr[i]>arr[i-1] && arr[i]>arr[i+1]){
+                                ans = i;
+                                break;
+                        }
+                }
+                if(ans==-1 && arr[n-1]>arr[n-2]) return n-1;
+                else return ans;
+        }
+
+        public static void main(String[] args){
+                Scanner sc = new Scanner(System.in);
+                int n = sc.nextInt();
+                int[] arr = new int[n];
+                for(int i=0;i<n;i++){
+                        arr[i] = sc.nextInt();
+                }
+                int idx = findPeak(arr,n);
+                System.out.println(idx);
+        }
+
 }
